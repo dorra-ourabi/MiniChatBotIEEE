4a8ef647d3dc511eb19890f304e085bc9010c5a2
dorra dorra <dorra.ourabi@insat.ucar.tn>
2026-01-16 19:52:12 +0100

    committing the first version of the project

---
+++ IEEChatBot.py
@@ -0,0 +1,30 @@
+from openai import OpenAI
+from foodForModel import BASE_MESSAGES
+import os
+
+
+client = OpenAI(
+    api_key=os.getenv("OPENAI_API_KEY"),
+    base_url="https://openrouter.ai/api/v1"
+)
+
+
+
+print("- Hello is there anything you want to know about IEEE INSAT?")
+while True:
+     n=input("- ")
+     if n in ["Goodbye","quit"]:
+         print("Goodbye")
+         break
+
+     m={"role":"user","content":n}
+     messages=BASE_MESSAGES+[m]
+     response = client.chat.completions.create(
+        model="mistralai/mistral-7b-instruct",
+        messages= messages,
+        temperature=0,
+
+        )
+
+     print ("-"+response.choices[0].message.content)
+
---
+++ foodForModel.py
@@ -0,0 +1,65 @@
+BASE_MESSAGES = [
+    {
+        "role": "system",
+        "content": (
+            "You are a friendly assistant. "
+            "Answer ONLY the current question. "
+            " this is important : Summarize your answer "
+            "Do NOT mention or repeat previous answers."
+        )
+    },
+    {
+        "role": "assistant",
+        "content": "We are IEEE INSAT Student Branch, an energetic community devoted to technological advancement. Rooted in the renowned IEEE, a global force for innovation, we're part of IEEE Region 8, linking us to a vast network across Europe, Africa, and the Middle East. Locally, we're represented by the IEEE Tunisia Section, fostering collaboration and knowledge exchange. INSAT provides the nurturing environment where our ideas flourish into meaningful projects. Together, we push boundaries and inspire the next generation of innovators.."
+    },
+    {
+        "role": "assistant",
+        "content": "INSAT: National Institute of Applied Science and Technology The National Institute of Applied Science and Technology (INSAT) is a Tunisian engineering school located in Tunis. It is one of the most prestigious engineering schools in Tunisia. INSAT is a public institution of higher education and scientific research. It is a member of the University of Carthage.."
+    },
+    {
+        "role": "assistant",
+        "content": "TSYP is the Tunisian Students and Young Professionals contest."
+    },
+    {
+        "role": "assistant",
+        "content": "IEEE INSAT was founded in 2013."
+    },
+    {
+        "role": "assistant",
+        "content": "some  of the latest awrads they received: Best RAS Student Branch Chapter This the 2nd time in a row to win this award . we are so thankful for all members and RAS Board for the last mandates 2025-11-09"
+        "Most Creative Event Award Awarded to IEEE INSAT PES–PELS Joint Chapter for organizing NRTF 2.0, recognized for its innovative concept, creative execution, and outstanding engagement that set a new benchmark for technical events. 2025-09-21"
+        "Most Oustanding Medium PES SBC in Tunisia 2024 The PES–PELS Chapter distinguished itself in 2024 through high-impact technical activities, strong member engagement, and consistent contributions to the power and energy community. This award recognizes its excellence, leadership, and meaningful impact at the national level.2024-12-21"
+        "Best RAS Student Branch Chapter In its inaugural year, the RAS Chapter stood out through exceptional initiative, impactful activities, and strong engagement. This award recognizes its remarkable achievements as a newly established chapter and the solid foundation it has built for future excellence. 2024-10-19"
+        "IEEE Region 8 Student Branch of the Year Award 2022 2nd Place 2024-02-29"
+        "2023 IEEE Regional Exemplary Student Branch Award 2024-02-09"
+
+    },
+    {"role":"assistant",
+     "content":"the chapters of IEEE INSAT:"
+     "IEEE Robotics & Automation Society INSAT SBC"
+     "IEEE Industrial Application Society INSAT SBC"
+     "IEEE Computer Society INSAT SBC"
+     "IEEE Women in Engineering INSAT AG"
+      "IEEE Engineering in Medicine & Biology Society INSAT SBC"
+     "IEEE Power & Energy and Power Electronics INSAT Joint SBC"
+      "IEEE Special Interest Group on Humanitarian Technology"
+
+     },
+    {"role":"assistant",
+     "content":" events held by the IEEE INSAT and their dates:"
+     "DataOverflow:DO is a cycle of trainings that will be concluded with a hackathon in the field of Data Science & AI , this event is a collaboration between IEEE INSAT SB and ACM INSAT : jan -feb"
+     "Wellness Innovation Bootcamp"
+     "Junior Enlightment Day"
+     "IEEE Sparkle"
+     "Student Professional Awareness: SuitUp"
+     "Summer School"
+     "Artificial Intelligence National Summit"
+     "EcoThoughts Bootcamp"
+     "National Re-Tech Fusion: NRTF"
+     "Pinktober"
+     "National Robotics Weekend"
+     "ORBIT"
+     "Embedded Competitive Programming Preparations"
+     "RAS ODYSSEY"
+     }
+]

60d2fe736e1abca642a7b6d7c9e149f0878a7946
dorra dorra <dorra.ourabi@insat.ucar.tn>
2026-03-22 16:35:09 +0100

    implemented the design pattern sigleton for the class Client

--- ClientClass.py
+++ ClientClass.py
@@ -1,12 +1,23 @@
 from openai import OpenAI
 import os
 class Client:
+    instance=None
+    client_created=False
+    def __new__(cls, *args, **kwargs):
+        if not cls.instance:
+            cls.instance = object.__new__(cls)
+        return cls.instance
     def __init__(self,api_key,base_url):
         self.__api_key = api_key
         self.__base_url = base_url
-    def make_client(self):
-        client = OpenAI(api_key=os.getenv(self.__api_key),base_url=self.__base_url)
-        return client
+
+        if  not self.client_created:
+            self.__client = OpenAI(api_key=os.getenv(self.__api_key), base_url=self.__base_url)
+            self.client_created=True
+
+    def get_client(self):
+
+        return self.__client
 
 
 
